8c0272b108a54f5a89142d2b5fafec349f845dcb
hithere <wongsiulunroy@gmail.com>
2019-11-13 23:06:41 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,12 @@
+<script>
+for(var i = 0; i <= 20 ; i++){
+	if(i%2==0){
+		document.getElementById("demo").innerHTML+= i + " is an even number. <br>" ;
+	}
+	else{
+		document.getElementById("demo").innerHTML+= i + " is an odd number. <br>" ;
+	}
+
+}
+
+</script>
